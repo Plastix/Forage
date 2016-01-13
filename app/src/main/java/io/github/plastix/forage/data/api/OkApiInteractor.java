9cb186b998d042b8fb54dfcd6bcfbc44cd320cd3
Plastix <aidanpieper@gmail.com>
2016-01-13 14:20:55 -0500

    OkApi is the new OkApiInteractor

--- app/src/main/java/io/github/plastix/forage/data/api/OkApiInteractor.java
+++ app/src/main/java/io/github/plastix/forage/data/api/OkApiInteractor.java
@@ -3,30 +3,45 @@ package io.github.plastix.forage.data.api;
 import android.location.Location;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import io.github.plastix.forage.data.UseCase;
-import rx.SingleSubscriber;
+import io.github.plastix.forage.BuildConfig;
+import io.github.plastix.forage.util.JsonUtils;
+import rx.Single;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
+/**
+ * A light wrapper around {@link OkApiService}.
+ */
 @Singleton
-public class OkApiInteractor extends UseCase {
+public class OkApiInteractor {
 
-    private OkApi api;
+    private OkApiService apiService;
 
     @Inject
-    public OkApiInteractor(OkApi api) {
-        this.api = api;
+    public OkApiInteractor(OkApiService apiService) {
+        this.apiService = apiService;
     }
 
-    public void getNearbyCaches(Location location, SingleSubscriber<JsonArray> subscriber) {
-        this.subscription = api.getNearbyCaches(location)
-                .observeOn(Schedulers.io())
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+    public Single<JsonArray> getNearbyCaches(Location location) {
+        return apiService.getNearbyCaches(
+                OkApiService.ENDPOINT_NEAREST,
+                String.format("{\"center\":\"%s|%s\"}", location.getLatitude(), location.getLongitude()),
+                OkApiService.ENDPOINT_GEOCACHES,
+                "{\"fields\":\"code|name|location|type|status|terrain|difficulty|size2|description\"}",
+                false,
+                BuildConfig.OKAPI_US_CONSUMER_KEY
+        ).map(new Func1<JsonObject, JsonArray>() { // TODO Replace this map with a Retrofit converter?
+            @Override
+            public JsonArray call(JsonObject jsonObject) {
+                return JsonUtils.jsonObjectToArray(jsonObject);
+            }
+        }).observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread());
     }
-
 }
