7120e3c3715157808db6d440a932e1183a58d6ab
Plastix <aidanpieper@gmail.com>
2016-03-24 12:56:49 -0400

    Add NetworkInteractor unit tests

--- app/src/unitTests/java/io/github/plastix/forage/data/network/NetworkInteractorTest.java
+++ app/src/unitTests/java/io/github/plastix/forage/data/network/NetworkInteractorTest.java
@@ -1,4 +1,66 @@
 package io.github.plastix.forage.data.network;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import rx.Completable;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class NetworkInteractorTest {
+
+    private NetworkInteractor networkInteractor;
+    private ConnectivityManager connectivityManager;
+    private NetworkInfo networkInfo;
+
+    @Before
+    public void beforeEachTest() {
+        networkInfo = mock(NetworkInfo.class);
+        connectivityManager = mock(ConnectivityManager.class);
+        networkInteractor = new NetworkInteractor(connectivityManager);
+    }
+
+    @Test
+    public void hasInternetConnection_shouldReturnFalseWhenNoNetwork() {
+        when(connectivityManager.getActiveNetworkInfo()).thenReturn(null);
+
+        assertThat(networkInteractor.hasInternetConnection()).isFalse();
+    }
+
+    @Test
+    public void hasInternetConnection_shouldReturnFalseWhenNotConnected() {
+        when(networkInfo.isConnectedOrConnecting()).thenReturn(false);
+        when(connectivityManager.getActiveNetworkInfo()).thenReturn(networkInfo);
+
+        assertThat(networkInteractor.hasInternetConnection()).isFalse();
+    }
+
+    @Test
+    public void hasInternetConnection_shouldReturnTrueWhenConnected() {
+        when(networkInfo.isConnectedOrConnecting()).thenReturn(true);
+        when(connectivityManager.getActiveNetworkInfo()).thenReturn(networkInfo);
+
+        assertThat(networkInteractor.hasInternetConnection()).isTrue();
+    }
+
+    @Test
+    public void hasInternetConnectionCompletable_shouldCompleteWhenConnected() {
+        when(networkInfo.isConnectedOrConnecting()).thenReturn(true);
+        when(connectivityManager.getActiveNetworkInfo()).thenReturn(networkInfo);
+
+        assertThat(networkInteractor.hasInternetConnectionCompletable()).isEqualTo(Completable.complete());
+    }
+
+    @Test
+    public void hasInternetConnectionCompletable_shouldErrorWhenNotConnected() {
+        when(networkInfo.isConnectedOrConnecting()).thenReturn(false);
+        when(connectivityManager.getActiveNetworkInfo()).thenReturn(networkInfo);
+
+        assertThat(networkInteractor.hasInternetConnectionCompletable().get()).isInstanceOf(Throwable.class);
+    }
 }
