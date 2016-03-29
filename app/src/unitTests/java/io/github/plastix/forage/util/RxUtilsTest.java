bd2155709c69b2549c9b43ef4271221933411301
Plastix <aidanpieper@gmail.com>
2016-03-29 09:54:32 -0400

    Add unit tests for RxUtils.safeUnsubscribe

--- app/src/unitTests/java/io/github/plastix/forage/util/RxUtilsTest.java
+++ app/src/unitTests/java/io/github/plastix/forage/util/RxUtilsTest.java
@@ -1,4 +1,43 @@
 package io.github.plastix.forage.util;
 
+import org.junit.Test;
+
+import rx.Subscription;
+
+import static com.google.common.truth.Truth.assert_;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class RxUtilsTest {
+
+    @Test
+    public void safeUnsubscribe_handleNullSubscription() {
+        try {
+            RxUtils.safeUnsubscribe(null);
+        } catch (Exception e) {
+            assert_().fail("RxUtils safeUnsubscribe threw an unexpected error!", e);
+        }
+    }
+
+    @Test
+    public void safeUnsubscribe_unsubscribeSubscriptionCorrectly() {
+        Subscription subscription = mock(Subscription.class);
+
+        RxUtils.safeUnsubscribe(subscription);
+
+        verify(subscription, times(1)).unsubscribe();
+    }
+
+    @Test
+    public void safeUnsubscribe_onlyUnsubscribeActiveSubscriptions() {
+        Subscription subscription = mock(Subscription.class);
+        when(subscription.isUnsubscribed()).thenReturn(true);
+
+        RxUtils.safeUnsubscribe(subscription);
+
+        verify(subscription, never()).unsubscribe();
+    }
 }
