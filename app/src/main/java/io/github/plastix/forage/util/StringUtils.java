6b9380a0127f59ceb6996f8379db94a74bbf48d6
Plastix <aidanpieper@gmail.com>
2016-03-30 07:12:32 -0400

    Add partial unit test coverage for StringUtils

--- app/src/main/java/io/github/plastix/forage/util/StringUtils.java
+++ app/src/main/java/io/github/plastix/forage/util/StringUtils.java
@@ -54,6 +54,11 @@ public class StringUtils {
      * @return Capitalized string.
      */
     public static String capitalize(@NonNull String input) {
+        // Don't process empty strings
+        if (input.length() == 0) {
+            return input;
+        }
+
         return input.substring(0, 1).toUpperCase(Locale.getDefault()) + input.substring(1);
     }
 
--- app/src/unitTests/java/io/github/plastix/forage/util/StringUtilsTest.java
+++ app/src/unitTests/java/io/github/plastix/forage/util/StringUtilsTest.java
@@ -1,4 +1,157 @@
 package io.github.plastix.forage.util;
 
+import android.content.res.Resources;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.github.plastix.forage.ForageRoboelectricUnitTestRunner;
+import io.github.plastix.forage.R;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class StringUtilsTest {
+
+    private static Resources resources;
+
+    /**
+     * Runs once when the unit test class loads. Set up some objects to be reused by tests.
+     */
+    @BeforeClass
+    public static void onlyOnce() {
+        resources = mock(Resources.class);
+        when(resources.getString(R.string.unit_feet)).thenReturn("ft");
+        when(resources.getString(R.string.unit_miles)).thenReturn("mi");
+    }
+
+
+    //
+    // Unit tests for join()
+    //
+
+    @Test
+    public void join_shouldJoinStrings() {
+        String[] input = {"a", "b", "c"};
+        String delimiter = ",";
+        String result = StringUtils.join(delimiter, input);
+
+        assertThat(result).isEqualTo("a,b,c");
+    }
+
+    @Test
+    public void join_joinsTwoLongList() {
+        String[] input = {"a", "b",};
+        String delimiter = ",";
+        String result = StringUtils.join(delimiter, input);
+
+        assertThat(result).isEqualTo("a,b");
+    }
+
+    @Test
+    public void join_joinsOneLongList() {
+        String[] input = {"a"};
+        String delimiter = ",";
+        String result = StringUtils.join(delimiter, input);
+
+        assertThat(result).isEqualTo("a");
+    }
+
+    @Test
+    public void join_joinsWithEmptyDelimiter() {
+        String[] input = {"a", "b", "c"};
+        String delimiter = "";
+        String result = StringUtils.join(delimiter, input);
+
+        assertThat(result).isEqualTo("abc");
+    }
+
+    @Test
+    public void join_emptyListReturnsEmptyString() {
+        String[] input = {};
+        String delimiter = ",";
+        String result = StringUtils.join(delimiter, input);
+
+        assertThat(result).isEqualTo("");
+    }
+
+    //
+    // Unit tests for stripHtml()
+    //
+    // TODO Needs to be refactored since it depends on Android resources.
+
+    //
+    // Unit tests for capitalize()
+    //
+
+    @Test
+    public void capitalize_regularStringCapitalized() {
+        String input = "hello world!";
+        String expected = "Hello world!";
+
+        assertThat(StringUtils.capitalize(input)).isEqualTo(expected);
+    }
+
+    @Test
+    public void capitalize_alreadyCapitalString() {
+        String input = "Hello world!";
+        String expected = "Hello world!";
+
+        assertThat(StringUtils.capitalize(input)).isEqualTo(expected);
+    }
+
+    @Test
+    public void capitalize_singleCharString() {
+        String input = "a";
+        String expected = "A";
+
+        assertThat(StringUtils.capitalize(input)).isEqualTo(expected);
+
+    }
+
+    @Test
+    public void capitalize_emptyString() {
+        String input = "";
+
+        assertThat(StringUtils.capitalize(input)).isEqualTo(input);
+    }
+
+    //
+    // Unit tests for humanReadableImperialDistance()
+    //
+
+
+    @Test
+    public void humanReadableImperialDistance_showMiles() {
+        double dist = 1.5;
+        String expected = "1.50 mi";
+
+        assertThat(StringUtils.humanReadableImperialDistance(resources, dist)).isEqualTo(expected);
+    }
+
+    @Test
+    public void humanReadableImperialDistance_showFeetWhenLessThanOneMile() {
+        double dist = 0.5;
+        String expected = "2640.00 ft";
+
+        assertThat(StringUtils.humanReadableImperialDistance(resources, dist)).isEqualTo(expected);
+    }
+
+    @Test
+    public void humanReadableImperialDistance_zeroMiles() {
+        double dist = 0;
+        String expected = "0.00 ft";
+
+        assertThat(StringUtils.humanReadableImperialDistance(resources, dist)).isEqualTo(expected);
+    }
+
+    @Test
+    public void humanReadableImperialDistance_oneMileDisplaysInFeet() {
+        double dist = 1;
+        String expected = "5280.00 ft";
+
+        assertThat(StringUtils.humanReadableImperialDistance(resources, dist)).isEqualTo(expected);
+    }
 }
