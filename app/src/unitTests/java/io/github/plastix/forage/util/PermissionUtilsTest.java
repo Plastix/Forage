338e141494a2fef7b8a322d0478d51e6cabd08aa
Plastix <aidanpieper@gmail.com>
2016-04-17 20:59:40 -0400

    Add a few PermissionUtils unit tests

--- app/src/unitTests/java/io/github/plastix/forage/util/PermissionUtilsTest.java
+++ app/src/unitTests/java/io/github/plastix/forage/util/PermissionUtilsTest.java
@@ -1,4 +1,42 @@
 package io.github.plastix.forage.util;
 
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
 public class PermissionUtilsTest {
+
+    @Test
+    public void isPermissionRequestCancelled_shouldBeCancelled() {
+        assertThat(PermissionUtils.isPermissionRequestCancelled(new int[]{})).isTrue();
+    }
+
+    @Test
+    public void isPermissionRequestCancelled_isNotCancelled() {
+        assertThat(PermissionUtils.isPermissionRequestCancelled(new int[]{-1, -1})).isFalse();
+        assertThat(PermissionUtils.isPermissionRequestCancelled(new int[]{0, 0})).isFalse();
+        assertThat(PermissionUtils.isPermissionRequestCancelled(new int[]{0, -1})).isFalse();
+        assertThat(PermissionUtils.isPermissionRequestCancelled(new int[]{-1, 0})).isFalse();
+        assertThat(PermissionUtils.isPermissionRequestCancelled(new int[]{-1})).isFalse();
+        assertThat(PermissionUtils.isPermissionRequestCancelled(new int[]{0})).isFalse();
+
+
+    }
+
+    @Test
+    public void hasAllPermissionsGranted_allGranted() {
+        assertThat(PermissionUtils.hasAllPermissionsGranted(new int[]{0})).isTrue();
+        assertThat(PermissionUtils.hasAllPermissionsGranted(new int[]{0, 0})).isTrue();
+        assertThat(PermissionUtils.hasAllPermissionsGranted(new int[]{0, 0, 0})).isTrue();
+    }
+
+
+    @Test
+    public void hasAllPermissionsGranted_notAllGranted() {
+        assertThat(PermissionUtils.hasAllPermissionsGranted(new int[]{})).isFalse();
+        assertThat(PermissionUtils.hasAllPermissionsGranted(new int[]{-1})).isFalse();
+        assertThat(PermissionUtils.hasAllPermissionsGranted(new int[]{0, -1})).isFalse();
+        assertThat(PermissionUtils.hasAllPermissionsGranted(new int[]{0, -1, 0})).isFalse();
+        assertThat(PermissionUtils.hasAllPermissionsGranted(new int[]{-1, -1, 0})).isFalse();
+    }
 }
