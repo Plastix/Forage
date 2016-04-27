b4d5a519a7299ca64d4b56148e7c9be28b141364
Plastix <aidanpieper@gmail.com>
2016-04-27 11:02:22 -0400

    Use Espresso Intents in CacheList UI test

--- app/src/androidTest/java/io/github/plastix/forage/tests/CacheListTest.java
+++ app/src/androidTest/java/io/github/plastix/forage/tests/CacheListTest.java
@@ -1,8 +1,9 @@
 package io.github.plastix.forage.tests;
 
+import android.content.ComponentName;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 
@@ -15,12 +16,15 @@ import io.github.plastix.forage.R;
 import io.github.plastix.forage.screens.CacheListScreen;
 import io.github.plastix.forage.screens.MapScreen;
 import io.github.plastix.forage.ui.cachelist.CacheListActivity;
+import io.github.plastix.forage.ui.map.MapActivity;
 import io.github.plastix.forage.util.TestUtils;
 import io.github.plastix.forage.util.UiAutomatorUtils;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
@@ -29,8 +33,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 public class CacheListTest {
 
     @Rule
-    public ActivityTestRule<CacheListActivity> activityTestRule =
-            new ActivityTestRule<>(CacheListActivity.class);
+    public IntentsTestRule<CacheListActivity> activityTestRule =
+            new IntentsTestRule<>(CacheListActivity.class);
 
     private CacheListScreen cacheListScreen;
     private MapScreen mapScreen;
@@ -41,8 +45,6 @@ public class CacheListTest {
         cacheListScreen = new CacheListScreen();
         mapScreen = new MapScreen();
         device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-
-
     }
 
     @Test
@@ -57,8 +59,13 @@ public class CacheListTest {
     public void testMapButton() {
         UiAutomatorUtils.allowPermissionsIfNeeded(device);
 
+        // Opens MapActivity
         onView(withId(R.id.cachelist_action_map)).perform(click());
 
+        // Verify that an MapActivity intent has been launched
+        intended(hasComponent(new ComponentName(InstrumentationRegistry.getTargetContext(), MapActivity.class)));
+
+        // Verify the MapActivity toolbar title
         mapScreen.shouldDisplayTitle(TestUtils.app().getString(R.string.map_title));
     }
 
