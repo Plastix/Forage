ca4137d5b7aa2fa24876ca0f9b06fe63140d52ff
Plastix <aidanpieper@gmail.com>
2016-05-15 13:50:05 -0400

    Add view NPE checks to LoginPresenter

--- app/src/main/java/io/github/plastix/forage/ui/login/LoginPresenter.java
+++ app/src/main/java/io/github/plastix/forage/ui/login/LoginPresenter.java
@@ -9,49 +9,69 @@ import io.github.plastix.forage.ui.Presenter;
 import rx.SingleSubscriber;
 import rx.functions.Action0;
 import rx.functions.Action1;
+import rx.subscriptions.CompositeSubscription;
 
 public class LoginPresenter extends Presenter<LoginView> {
 
     private OAuthInteractor oAuthInteractor;
+    private CompositeSubscription subscriptions;
 
     @Inject
     public LoginPresenter(OAuthInteractor oAuthInteractor) {
         this.oAuthInteractor = oAuthInteractor;
+        subscriptions = new CompositeSubscription();
     }
 
     public void startOAuth() {
-        oAuthInteractor.retrieveRequestToken().subscribe(new SingleSubscriber<String>() {
-            @Override
-            public void onSuccess(String value) {
-                view.openBrowser(value);
-            }
+        subscriptions.add(
+                oAuthInteractor.retrieveRequestToken().subscribe(new SingleSubscriber<String>() {
+                    @Override
+                    public void onSuccess(String value) {
+                        if (isViewAttached()) {
+                            view.openBrowser(value);
+                        }
+                    }
 
-            @Override
-            public void onError(Throwable error) {
-                view.onErrorRequestToken();
-            }
-        });
-
-
+                    @Override
+                    public void onError(Throwable error) {
+                        if (isViewAttached()) {
+                            view.onErrorRequestToken();
+                        }
+                    }
+                })
+        );
     }
 
     public void oauthCallback(final Uri uri) {
-        oAuthInteractor.retrieveAccessToken(uri)
-                .subscribe(new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        view.onErrorAccessToken();
-                    }
-                }, new Action0() {
-                    @Override
-                    public void call() {
-                        view.onAuthSuccess();
-                    }
-                });
+        subscriptions.add(
+                oAuthInteractor.retrieveAccessToken(uri)
+                        .subscribe(new Action1<Throwable>() {
+                            @Override
+                            public void call(Throwable throwable) {
+                                if (isViewAttached()) {
+                                    view.onErrorAccessToken();
+                                }
+                            }
+                        }, new Action0() {
+                            @Override
+                            public void call() {
+                                if (isViewAttached()) {
+                                    view.onAuthSuccess();
+                                }
+                            }
+                        })
+        );
+    }
+
+    @Override
+    public void onViewDetached() {
+        super.onViewDetached();
+        subscriptions.clear();
     }
 
     @Override
     public void onDestroyed() {
         oAuthInteractor = null;
+        subscriptions = null;
     }
 }
