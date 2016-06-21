d72ea20a44197348546b36e0b561ca7cec40a19c
Plastix <aidanpieper@gmail.com>
2016-06-21 07:39:16 -0400

    Clean up some Api Constants

--- app/src/main/java/io/github/plastix/forage/data/api/ApiConstants.java
+++ app/src/main/java/io/github/plastix/forage/data/api/ApiConstants.java
@@ -6,13 +6,13 @@ package io.github.plastix.forage.data.api;
  */
 public class ApiConstants {
 
-    public static final String BASE_ENDPOINT = "http://www.opencaching.us";
-    public static final String REQUEST_TOKEN_ENDPOINT = BASE_ENDPOINT + "/okapi/services/oauth/request_token";
-    public static final String ACCESS_TOKEN_ENDPOINT = BASE_ENDPOINT + "/okapi/services/oauth/access_token";
-    public static final String AUTHORIZATION_WEBSITE_URL = BASE_ENDPOINT + "/okapi/services/oauth/authorize";
-    public static final String OAUTH_CALLBACK = "app://forage";
-    public static final String OAUTH_ENABLE_HEADER = "OAuth";
-
+    public static final String BASE_ENDPOINT = "http://www.opencaching.us/okapi/";
+    public static final String REQUEST_TOKEN_ENDPOINT = BASE_ENDPOINT + "services/oauth/request_token";
+    public static final String ACCESS_TOKEN_ENDPOINT = BASE_ENDPOINT + "services/oauth/access_token";
+    public static final String AUTHORIZATION_WEBSITE_URL = BASE_ENDPOINT + "services/oauth/authorize";
     public static final String ENDPOINT_GEOCACHES = "services/caches/geocaches";
     public static final String ENDPOINT_NEAREST = "services/caches/search/nearest";
+
+    public static final String OAUTH_CALLBACK = "app://forage";
+    public static final String OAUTH_ENABLE_HEADER = "OAuth";
 }
