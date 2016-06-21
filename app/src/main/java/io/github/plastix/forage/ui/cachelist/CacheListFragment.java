dfd512a4b75e49466c85f41cdeb52608c4103426
Plastix <aidanpieper@gmail.com>
2016-06-21 07:25:48 -0400

    Add TODO about SwipeRefreshLayout Support bug

--- app/src/main/java/io/github/plastix/forage/ui/cachelist/CacheListFragment.java
+++ app/src/main/java/io/github/plastix/forage/ui/cachelist/CacheListFragment.java
@@ -103,6 +103,9 @@ public class CacheListFragment extends PresenterFragment<CacheListPresenter, Cac
 
     @Override
     public void setRefreshing() {
+        // TODO: Post fix required due to Support V4 bug
+        // Will be fixed in 24.1.0
+        // See https://code.google.com/p/android/issues/detail?id=77712
         swipeRefreshLayout.post(() -> swipeRefreshLayout.setRefreshing(true));
     }
 
