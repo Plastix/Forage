9346283cd6ace64ba5d98ec356744b13c56b9bcb
Plastix <aidanpieper@gmail.com>
2016-06-26 09:12:02 -0400

    Last ditch effort on new ci.sh

--- ci.sh
+++ ci.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-# Run it from root project directory!
+set -xe
 
-# This command does the following:
-# - Builds the project
-# - Runs Android Lint
-# - Runs Unit Tests under JVM
-# - Packages the APK
-./gradlew clean build test -PdisablePreDex
+# You can run it from any directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# This will:
+#    1. Clean the project
+#    2. Run Android Lint
+#    3. Run tests under JVM
+
+# We run each step separately to consume less memory (free CI sometimes fails with OOM)
+
+GRADLE=""$DIR"/gradlew -PdisablePreDex --no-daemon"
+
+# 1
+eval "$GRADLE clean"
+
+# 2
+eval "$GRADLE lintDebug"
+eval "$GRADLE lintRelease"
+
+# 3
+eval "$GRADLE testDebugUnitTest"
+eval "$GRADLE testReleaseUnitTest"
